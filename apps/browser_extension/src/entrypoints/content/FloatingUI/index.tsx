e351ea427402ec7e7ad7b8fabc8434b6028f9ec4
ymrl <ymrl@ymrl.net>
2025-10-12 11:38:26 +0900

    change iframe creation to react component

--- apps/browser_extension/src/entrypoints/content/FloatingUI/index.tsx
+++ apps/browser_extension/src/entrypoints/content/FloatingUI/index.tsx
@@ -1,2 +1,1 @@
 export * from "./FloatingUIRoot";
-export * from "./initFloatingUI";
--- apps/browser_extension/src/entrypoints/content/Root.tsx
+++ apps/browser_extension/src/entrypoints/content/Root.tsx
@@ -6,15 +6,11 @@ import {
   useEffect,
   useRef,
 } from "react";
-import { createPortal } from "react-dom";
 import { createRoot } from "react-dom/client";
 import { MessageResponder } from "./ExtensionContext/MessageResponder";
-import {
-  type FloatingUIHandle,
-  FloatingUIRoot,
-  initFloatingUI,
-} from "./FloatingUI";
+import { type FloatingUIHandle, FloatingUIRoot } from "./FloatingUI";
 import { FrameManager } from "./FrameManager";
+import { Iframe } from "./Iframe";
 import { LandmarkNavigation } from "./LandmarkNavigation";
 import { Speaker } from "./Speaker";
 import { TextStyleTweaker } from "./TextStyleTweaker";
@@ -27,21 +23,20 @@ export const createRootElement = () => {
   const root = document.createElement("div");
   parent.appendChild(root);
   const reactRoot = createRoot(root);
-  const { iframeRoot } = initFloatingUI({ parent });
   const rootRef = createRef<HTMLDivElement | null>();
-  const iframeRootRef = createRef<HTMLDivElement | null>();
-  iframeRootRef.current = iframeRoot || null;
   rootRef.current = root;
-  reactRoot.render(createElement(Root, { rootRef, iframeRootRef }));
+  reactRoot.render(
+    createElement(Root, {
+      rootRef,
+    }),
+  );
   return root;
 };
 
 export const Root = ({
   rootRef,
-  iframeRootRef,
 }: {
   rootRef: RefObject<HTMLElement | null>;
-  iframeRootRef: RefObject<HTMLElement | null>;
 }) => {
   const handleRef = useRef<FloatingUIHandle | null>(null);
 
@@ -63,12 +58,9 @@ export const Root = ({
       <TextStyleTweaker />
       <LandmarkNavigation rootRef={rootRef} />
       <Speaker />
-
-      {iframeRootRef.current &&
-        createPortal(
-          <FloatingUIRoot handleRef={handleRef} windowHeight={windowHeight} />,
-          iframeRootRef.current,
-        )}
+      <Iframe>
+        <FloatingUIRoot handleRef={handleRef} windowHeight={windowHeight} />
+      </Iframe>
 
       <FrameManager>
         <TextStyleTweaker />
