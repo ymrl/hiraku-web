9b9c82b2596de55492eee7bdc154dbb59c595057
ymrl <ymrl@ymrl.net>
2025-10-27 22:13:35 +0900

    スイッチに使用するシンボルの変更

--- apps/browser_extension/src/components/Switch.tsx
+++ apps/browser_extension/src/components/Switch.tsx
@@ -24,7 +24,9 @@ export const Switch = ({
         : "bg-stone-600 hover:bg-stone-600 ") +
       "before:content-[''] before:absolute before:z-10 before:rounded-full before:h-5 before:w-5 before:transition-all " +
       "before:left-[2px] before:top-[2px] " +
-      "before:bg-stone-100 hover:before:bg-white " +
+      (isOn
+        ? "before:bg-white "
+        : "before:bg-stone-100 hover:before:bg-white ") +
       (isOn ? "before:translate-x-full before:border-white " : "")
     }
     aria-pressed={isOn}
@@ -33,10 +35,17 @@ export const Switch = ({
     <span
       aria-hidden="true"
       className={
-        "before:content-[''] before:absolute before:top-1.5 before:bottom-1.5 before:right-3 before:w-0 " +
+        "before:content-[''] before:absolute before:top-1.5 before:bottom-1.5 before:right-3 before:w-0 before:-rotate-45 " +
         "before:border-l-2 before:border-white before:opacity-80 " +
-        "after:content-[''] after:absolute after:top-1.5 after:left-1.5 after:w-3 after:h-3 " +
-        "after:border-2 after:rounded-full after:border-white after:opacity-80"
+        "after:content-[''] after:absolute after:top-1.5 after:bottom-1.5 after:right-3 after:w-0 after:rotate-45 " +
+        "after:border-l-2 after:border-white after:opacity-80"
+      }
+    />
+    <span
+      aria-hidden="true"
+      className={
+        "after:content-[''] after:absolute after:top-1.5 after:left-1.5 after:w-3 after:h-2 after:-rotate-45 " +
+        "after:border-2 after:border-l-white after:border-b-white after:border-t-transparent after:border-r-transparent after:opacity-80"
       }
     />
     {children && <span className="sr-only">{children}</span>}
